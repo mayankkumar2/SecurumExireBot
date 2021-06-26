8ab613daf1a2927ce2d69b3509777a3cb4f43421
Mayank Kumar <mayank22oct@gmail.com>
2021-06-26 15:24:07 +0530

    feat: added gorm

--- main.go
+++ main.go
@@ -1,19 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"net/http"
 	"os"
 )
 
+var DB *gorm.DB
+var token string
+type UserModel struct {
+	UserID uuid.UUID `json:"user_id" gorm:"type:uuid"`
+	ChatID int `json:"chat_id" gorm:"unique"`
+	AuthKey string `json:"-"`
+	WebHook string `json:"web_hook"`
+}
+
+func init()  {
+	_ = godotenv.Load(".env")
+	host := os.Getenv("HOST")
+	dbUser := os.Getenv("DB_USER")
+	userPass := os.Getenv("DB_USER_PASS")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	token = os.Getenv("BOT_TOKEN")
+	dbStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host,
+		dbUser,
+		userPass,
+		dbName,
+		port)
+	DB, err := gorm.Open(postgres.Open(dbStr), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	err = DB.AutoMigrate(&UserModel{})
+	if err != nil {
+		fmt.Println("Auto migration failed!")
+	}
+}
+
+type Update struct {
+	UpdateID int `json:"update_id"`
+	Message Message `json:"message"`
+}
+
+type Message struct {
+	Text string `json:"text"`
+}
+
 func main() {
 	port := os.Getenv("PORT")
-
 	http.HandleFunc("/bot", func(w http.ResponseWriter, r *http.Request) {
-		b, _ := ioutil.ReadAll(r.Body)
-		fmt.Println(string(b))
+		var u Update
 
+		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			return
+		}
+
+		fmt.Println(u)
 		_, _ = w.Write([]byte("Hi"))
 	})
 
