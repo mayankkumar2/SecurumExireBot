29ccbc09c39be4b3dd8046b059aadd9a06236c17
Mayank Kumar <mayank22oct@gmail.com>
2021-06-26 14:34:34 +0530

    feat: basic app

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module SecrurumExireBot
 
 go 1.16
+
+require gorm.io/gorm v1.21.11 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
 func main() {
-		
+	port := os.Getenv("PORT")
+
+	http.HandleFunc("/bot", func(w http.ResponseWriter, r *http.Request) {
+		b, _ := ioutil.ReadAll(r.Body)
+		fmt.Println(string(b))
+
+		_, _ = w.Write([]byte("Hi"))
+	})
+
+	fmt.Println("listening at port... " + port)
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Println("error: " + err.Error())
+	}
 }
