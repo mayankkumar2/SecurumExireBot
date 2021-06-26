f0c76989ebc50982194b2e349db4d81506c855fa
Mayank Kumar <mayank22oct@gmail.com>
2021-06-26 22:04:39 +0530

    feat: added report a leak route

--- main.go
+++ main.go
@@ -88,6 +88,35 @@ func main() {
 	initEnv()
 
 	port := os.Getenv("PORT")
+	http.HandleFunc("/report/leak", func(w http.ResponseWriter, r *http.Request) {
+		var requestBody struct{
+			Secret string `json:"secret"`
+			Endpoint string `json:"message"`
+			SecretName string `json:"secret_name"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		uid, err := AuthenticateRequestSecret(requestBody.Secret)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		var usr UserModel
+		err = DB.Where("user_id = ?", uid).First(&usr).Error
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		msgString := fmt.Sprintf("Attention! we were reported of a leak on endpoint[%s] for the secret[%s]",
+			requestBody.Endpoint,
+			requestBody.SecretName)
+
+		SendBotCommandMessage(usr.ChatID, msgString,"/block "+requestBody.Endpoint, "block endpoint")
+		w.WriteHeader(http.StatusOK)
+	})
 	http.HandleFunc("/bot", func(w http.ResponseWriter, r *http.Request) {
 		var u Update
 
